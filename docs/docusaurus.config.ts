f1c04797bcb9cc41885e5e76fc9862986ef2022a
Paulo Aragao <paragao@amazon.com>
2025-09-17 14:19:14 +0100

    fixing error

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -24,6 +24,8 @@ const config: Config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'awslabs', // Usually your GitHub org/user name.
   projectName: 'ai-on-sagemaker-hyperpod', // Usually your repo name.
+  deploymentBranch: 'gh-pages',
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
